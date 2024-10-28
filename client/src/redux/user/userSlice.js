f539f7c3cfcd19421367b6fc1136dc91dec513f2
Shihabsarker93 <shihabsarker600@gmail.com>
2024-10-28 18:20:07 +0600

    add sign out functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -46,6 +46,20 @@ const userSlice = createSlice({
       state.error = action.payload;
       state.loading = false;
     },
+    //signout front end
+    signOutUserStart: (state) => {
+      state.loading = true;
+    },
+    signOutUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    signOutUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+
   },
 });
 
@@ -59,6 +73,9 @@ export const {
   deleteUserFailure,
   deleteUserSuccess,
   deleteUserStart,
+  signOutUserFailure,
+  signOutUserSuccess,
+  signOutUserStart,
 } = userSlice.actions;
 
 export default userSlice.reducer;
