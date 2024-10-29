3b4eef07c50e1572ac8f6474c7740cb5f70d1cf5
Shihabsarker93 <shihabsarker600@gmail.com>
2024-10-29 13:24:58 +0600

    create get listings user api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,16 +1,13 @@
-// user.route.js
 import express from 'express';
-import { deleteUser, test, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, test, updateUser,  getUserListings} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
+
 
 const router = express.Router();
 
-// Test route
 router.get('/test', test);
-
-// Update user route
-router.put('/update/:id', verifyToken, updateUser); // Use PUT for updating
-router.delete('/delete/:id', verifyToken, deleteUser);
-
+router.post('/update/:id', verifyToken, updateUser)
+router.delete('/delete/:id', verifyToken, deleteUser)
+router.get('/listings/:id', verifyToken, getUserListings)
 
 export default router;
