1caa87cc676bd1777da592a434b4b6c9f625f596
Shihabsarker93 <shihabsarker600@gmail.com>
2024-10-29 18:32:17 +0600

    complete listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -101,7 +101,7 @@ export default function CreateListing() {
     }
 
     if (
-      e.target.id === 'parking' ||  
+      e.target.id === 'parking' ||
       e.target.id === 'furnished' ||
       e.target.id === 'offer'
     ) {
@@ -281,7 +281,9 @@ export default function CreateListing() {
               />
               <div className='flex flex-col items-center'>
                 <p>Regular price</p>
-                <span className='text-xs'>($ / month)</span>
+                {formData.type === 'rent' && (
+                  <span className='text-xs'>($ / month)</span>
+                )}
               </div>
             </div>
             {formData.offer && (
@@ -298,7 +300,10 @@ export default function CreateListing() {
                 />
                 <div className='flex flex-col items-center'>
                   <p>Discounted price</p>
-                  <span className='text-xs'>($ / month)</span>
+
+                  {formData.type === 'rent' && (
+                    <span className='text-xs'>($ / month)</span>
+                  )}
                 </div>
               </div>
             )}
