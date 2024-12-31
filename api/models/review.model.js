5edfe335eab0b2363ebbf5edbd5ad5a05a3c4c83
thewildwest12 <shihabsarker650@gmail.com>
2025-01-01 01:51:46 +0600

    feat/get-user-review

--- api/models/review.model.js
+++ api/models/review.model.js
@@ -27,6 +27,31 @@ const reviewSchema = new mongoose.Schema({
   index: { userRef: 1, listingRef: 1, unique: true }
 });
 
+// Add a pre-save middleware to enforce one review per user per listing
+reviewSchema.pre('save', async function(next) {
+  try {
+    // Check if a review already exists for this user and listing
+    const existingReview = await this.constructor.findOne({
+      userRef: this.userRef,
+      listingRef: this.listingRef,
+      _id: { $ne: this._id } // Exclude the current review when updating
+    });
+
+    if (existingReview) {
+      const error = new Error('You can only submit one review per listing');
+      error.name = 'ValidationError';
+      return next(error);
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+export default Review;
+
 
 
 
