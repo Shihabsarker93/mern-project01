5c715bd81e572d0b99bb2f01411843afdf892112
thewildwest12 <shihabsarker650@gmail.com>
2025-01-03 23:53:32 +0600

    feature/get-user-listing

---
+++ api/controllers/listing.controller.js
@@ -0,0 +1,67 @@
+import Listing from '../models/listing.model.js';
+import { errorHandler } from '../utils/error.js';
+
+//search api route backend listing.route.js
+export const getListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, 'Listing not found!'));
+    }
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+//seach api route backend listing.route.js->
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
+    //default behaviours 
+
+    if (offer === undefined || offer === 'false') {
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+
+    if (furnished === undefined || furnished === 'false') {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+
+    if (parking === undefined || parking === 'false') {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+
+    if (type === undefined || type === 'all') {
+      type = { $in: ['sale', 'rent'] };
+    }
+    //default values
+    const searchTerm = req.query.searchTerm || ''; //if no search ""
+
+    const sort = req.query.sort || 'createdAt';
+
+    const order = req.query.order || 'desc';
+    //search
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: 'i' }, // regex backend mongodb search functionality can search at anywehere options i lowercas
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex); //start from index given
+
+    return res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
