f9a076648f84e35429eaacadfdb1f71f710335e2
thewildwest12 <shihabsarker650@gmail.com>
2025-01-03 23:59:57 +0600

    feat/create-new-listing

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,6 +1,15 @@
 import Listing from '../models/listing.model.js';
 import { errorHandler } from '../utils/error.js';
 
+export const createListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.create(req.body);
+    return res.status(201).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
 //search api route backend listing.route.js
 export const getListing = async (req, res, next) => {
   try {
